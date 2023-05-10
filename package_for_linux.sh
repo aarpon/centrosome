5c597562d19c7eb41aa2d90bde4c8ba2eb60623d
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2023-05-10 11:16:57 +0200

    Fix build for Linux.

--- package_for_linux.sh
+++ package_for_linux.sh
@@ -33,8 +33,8 @@ source $ANACONDA_HOME/etc/profile.d/conda.sh
 conda create -n centrosome-build python=3.9 -y
 conda activate centrosome-build
 
-# Install Cython
-pip install cython
+# Install dependencies
+poetry install
 
 # Delete build and dist folders
 rm -fR build
@@ -58,8 +58,8 @@ conda env remove -n centrosome-build
 conda create -n centrosome-build python=3.10 -y
 conda activate centrosome-build
 
-# Install Cython
-pip install cython
+# Install dependencies
+poetry install
 
 # Delete build and dist folders
 rm -fR build
@@ -83,8 +83,8 @@ conda env remove -n centrosome-build
 conda create -n centrosome-build python=3.11 -y
 conda activate centrosome-build
 
-# Install Cython
-pip install cython
+# Install dependencies
+poetry install
 
 # Delete build and dist folders
 rm -fR build
