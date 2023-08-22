f00bc7e438e14b8e4486c2a3dffe24cc125f4156
Aaron Ponti <aarpon@gmail.com>
2023-08-22 15:05:29 +0200

    Fix cython version to <3.0.

--- setup.py
+++ setup.py
@@ -100,10 +100,9 @@ setuptools.setup(
         "Programming Language :: C++",
         "Programming Language :: Cython",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.11"
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Scientific/Engineering",
     ],
